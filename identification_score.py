612355e4f39aec51fc35fd2cdc7a16b457f8bbc7
malatesti <a.malatesti@studenti.unipi.it>
2024-02-24 00:53:53 +0100

    simplified score(accuracy) calculation

--- identification_score.py
+++ identification_score.py
@@ -1,43 +1,10 @@
-def id_score(possible_answers, guess, correct_answer):
-    """The agent guesses the possible objects that can have the given appearence.
-    This function returns the guess score
-    (this function is the same for all random appearences, so appearence doesn't have to be a function parameter)"""
-    assert correct_answer in possible_answers
-    assert all(a in possible_answers for a in guess)
-    assert len(guess)
-    if correct_answer not in guess:
-        return -10 # agent made a mistake
-    # the more accurate the guess, the closer the score is to +1
-    # if len(guess) == 1, then score = 1(the agent achieved perfect score)
-    # if len(guess) == len(possible_answers), then score = 0(the agent doesn't know useful information)
-    return (1 - (len(guess) - 1) / (len(possible_answers) - 1))**2
-
-def get_answer_bis(itm_mgr, appearence, treshold):
-    """answer the question: what objects have probability > treshold/N to have the given appearence?
-    N = number of possible objects that can have the given appearence"""
-    possibilities = itm_mgr.get_possible_objects(appearence)
-    return [o for p,o in possibilities if p > treshold / len(possibilities)]
-
-def get_answer_stoc(itm_mgr, appearence, treshold):
-    stoc = itm_mgr.get_stochastic(appearence)
-    possibilities = stoc.get_possible_objects(stoc.probabilities, appearence)
-    return [o for p,o in possibilities if p > treshold / len(possibilities)]
-    
-def total_score(itm_manager, correct_answers, probability_treshold):
-    """Get the final agent score, after the agent used itm_manager in an episode
+def total_score(item_manager, correct_answers, agent_answer):
+    """Get the final agent score, after the agent used item_manager in an episode.
     correct_answers is a dictionary where correct_answers[appearence] = object_name
-    probability_treshold(=pt) should be close to 0:
-    if pt == 0(too low), then consider objects that have even the smallest probability to have
-    a given appearence
-    if pt == 1(too high), then only consider objects that have higher-than-average probability
-    to have a given appearence
-    pt ~= 0.25 should be good enough"""
-    random_appearences = []
-    for (min_app, max_app) in itm_manager.appearence_ranges:
-        random_appearences.extend(range(min_app, max_app))
+    agent_answer(item_manager, appearence) = [list of possible objects that can have the given appearence]
+    """
     return sum(
-        id_score(itm_manager.get_stochastic(a).objects,
-                 get_answer_bis(itm_manager, a, probability_treshold),
-                 correct_answers[a])
-        for a in random_appearences
-        if a in correct_answers)
+        agent_answer(item_manager, a) == correct_answers[a]
+        for a in correct_answers.keys()
+    ) * 100 / len(correct_answers.keys())
+
